c1db75e77a57e1384dcdf24058bc993972761825
idilgunduz1 <idil.gunduz1@std.bogazici.edu.tr>
2025-03-07 16:30:43 +0300

    assignment idil gündüz

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
+
+    roman_dict = {
+        1000: 'M', 900: 'CM', 500: 'D', 400: 'CD',
+        100: 'C', 90: 'XC', 50: 'L', 40: 'XL',
+        10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'
+    }
+    if not 1 <= num <= 3999:
+        print("Number must be between 1 and 3999")
+    
+    roman_version = ""
+    for value, numeral in roman_dict.items():
+        while num >= value:
+            roman_version += numeral
+            num -= value
+    return roman_version
